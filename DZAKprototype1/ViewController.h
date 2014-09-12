3e2eb28d1ce5425fba37ad92d3b87a8aa889c9e2
Akash Kumar <akashk@dezinezync.com>
2014-09-12 13:20:42 +0530

    Initial commit

---
+++ DZAKprototype1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DZAKprototype1
+//
+//  Created by Akash Kumar on 9/12/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DZAKprototype1Tests/DZAKprototype1Tests.m
@@ -0,0 +1,34 @@
+//
+//  DZAKprototype1Tests.m
+//  DZAKprototype1Tests
+//
+//  Created by Akash Kumar on 9/12/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DZAKprototype1Tests : XCTestCase
+
+@end
+
+@implementation DZAKprototype1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
